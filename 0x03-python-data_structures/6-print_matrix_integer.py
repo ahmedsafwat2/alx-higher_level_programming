3dcb7bf46e95b484dfc037bc539a0e30c22f17c1
asosnses <asosnses@gmail.com>
2023-06-13 11:39:22 -0700

    asd

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     '''print all integers'''
     for i in matrix:
-        print(*i)
+        x = len(i) - 1
+        y = 0
+        for j in i:
+            print("{:d}".format(j), end=" " if y != x else "")
+            y += 1
+        print()
