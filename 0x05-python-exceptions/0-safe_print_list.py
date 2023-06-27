fe2a8468babefb349057de00321825a0fc87d5d7
asosnses <asosnses@gmail.com>
2023-06-26 19:44:25 -0700

    asd

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
 
@@ -17,4 +19,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (ret)
-
