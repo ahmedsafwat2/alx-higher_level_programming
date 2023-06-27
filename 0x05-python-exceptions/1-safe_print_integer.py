192789a737ccd9ad26cb5f926b98bdfa639f6c24
asosnses <asosnses@gmail.com>
2023-06-26 19:53:33 -0700

    asd

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -11,8 +11,8 @@ def safe_print_integer(value):
         If a TypeError or ValueError occurs - False.
         Otherwise - True.
     """
-try:
-    print("{:d}".format(value))
-    return (True)
-except (TypeError, ValueError):
-    return (False)
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
