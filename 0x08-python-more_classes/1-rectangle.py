a8b88c78441e8e4c5d9bb8bb57cc3926bdc0df3b
asosnses <asosnses@gmail.com>
2023-07-03 16:43:04 -0700

    asd

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             height (int): vertical dimension of rectangle, defaults to 0
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
