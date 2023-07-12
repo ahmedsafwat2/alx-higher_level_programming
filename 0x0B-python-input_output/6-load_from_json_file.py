4a08fdc6bbec25aa4e1248fb551f6b3f1bdf8d8f
asosnses <asosnses@gmail.com>
2023-07-11 19:42:03 -0700

    asd

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as myfile:
+    with open(filename) as myfile:
         return json.load(myfile)
