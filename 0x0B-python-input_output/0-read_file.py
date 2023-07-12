9b5cc66ebd14852e27e812b477c9f3e9cd695831
asosnses <asosnses@gmail.com>
2023-07-11 18:26:26 -0700

    asd

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,4 +2,4 @@
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, mode = "r", encoding = "utf-8") as myfile:
-        print(myfile.read())
+        print(myfile.read(),end = "")
