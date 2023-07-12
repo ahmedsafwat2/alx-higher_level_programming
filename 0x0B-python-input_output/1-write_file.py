f6f7db7a11d01bfb54966997c00dc2aeb4d78f2b
asosnses <asosnses@gmail.com>
2023-07-11 18:45:35 -0700

    asd

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define writing function"""
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+    Args:
+        filename(str): name of file to write.
+        text(str): text be written.
+        Returns:
+            number refer to character written.
+    """
+    with open(filename, "w", "utf-8") as myfile:
+        return (myfile.write(text))
