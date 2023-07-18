62830145b484673d3d490daf8671a009d24cc452
asosnses <asosnses@gmail.com>
2023-07-17 19:41:35 -0700

    asd

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -77,3 +77,17 @@ class Base:
                 f.write("[]")
             else:
                 json.dump(list_objs, f)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string.
+
+        Args:
+            json_string (str): A JSON str representation of a list of dicts.
+        Returns:
+            If json_string is None or empty - an empty list.
+            Otherwise - the Python list represented by json_string.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
