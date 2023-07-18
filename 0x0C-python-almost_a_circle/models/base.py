ba93c26d7da4c5a728314f76d67781c2297d3366
asosnses <asosnses@gmail.com>
2023-07-17 19:22:20 -0700

    asd

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -59,7 +59,7 @@ class Base:
         Args:
             list_dictionaries (list): A list of dictionaries.
         """
-        if len(list_dictionaries) == 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
